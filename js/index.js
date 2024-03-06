30c1da9f115c6c501e1721ff322ee98ce2035c45
Denys Kosyriev <utross2241@gmail.com>
2024-03-06 14:19:59 +0200

    feature: create header

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+        // Change height header while scroll
+        const body = document.getElementsByTagName("body")[0];
 
+        const handleScroll = () => {
+            if (window.scrollY < 60) {
+                body.classList.remove('scroll-top');
+            } else {
+                body.classList.add('scroll-top');
+            }
+        };
+        window.addEventListener("scroll", handleScroll);
 
     }
 )
