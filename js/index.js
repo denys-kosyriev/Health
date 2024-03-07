0bdb3727a2cd4132941ce54e755004a88f4f846f
Denys Kosyriev <utross2241@gmail.com>
2024-03-07 22:46:46 +0200

    feature: create bottom in meet section

--- js/index.js
+++ js/index.js
@@ -1,5 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+        // Change height header while scroll
+        const body = document.getElementsByTagName("body")[0];
+
+        const handleScroll = () => {
+            if (window.scrollY < 60) {
+                body.classList.remove('scroll-top');
+            } else {
+                body.classList.add('scroll-top');
+            }
+        };
+        window.addEventListener("scroll", handleScroll);
+
         // Close meet section
 
         const meet = document.getElementsByClassName("meet")[0];
@@ -8,6 +20,24 @@ document.addEventListener('DOMContentLoaded', function () {
         meetCloseBtn.addEventListener('click', () => {
             meet.classList.add('close-meet');
         })
+
+        // Play meet's video
+
+        const meetVideo = document.getElementsByClassName('meet-video');
+        const playIcon = document.getElementsByClassName('play-icon');
+        let video = document.getElementById('video');
+
+        let playVideo = new MediaElementPlayer(video, {
+            success: function (mediaElement, originalNode) {
+            }
+        });
+
+        for (let i = 0; i < playIcon.length; i++) {
+            playIcon[i].addEventListener('click', () => {
+                playVideo.play();
+                meetVideo[i].classList.add('play-video');
+            })
+        }
 
     }
 )
